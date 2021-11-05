c608fdbc0a15c6cfd019a6ece60605a89a71a7d9
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2021-11-05 19:48:03 -0300

    salvando

--- pages/components/layout/hero.vue
+++ pages/components/layout/hero.vue
@@ -3,7 +3,7 @@
     <!-- side nav -->
     <div class="lg:w-1/5 border-r border-lighter px-2 lg:px-6 py-2 flex flex-col justify-between">
       <div>
-        <button class="h-12 w-12 hover:bg-lightblue text-3xl rounded-full text-blue">
+        <button class="h-12 w-12 hover:bg-lightblue text-3xl rounded-full text-dark">
           <i class="fab fa-twitter"></i>
         </button>
         <div>
